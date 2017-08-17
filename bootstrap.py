15c7525907a020253500e129cbab21d565e15d22
vanrysss <samvanryssegem@gmail.com>
2017-08-16 18:02:53 -0700

    bootstrap script complete

--- bootstrap.py
+++ bootstrap.py
@@ -15,14 +15,14 @@ with open('sql/station/insert_station.sql', 'r') as station_insert_file:
     station_insert_query = station_insert_file.read()
 with open('sql/station/query_stations_by_system.sql', 'r') as station_query_file:
     station_query = station_query_file.read()
-
+with open('sql/item/insert_item.sql', 'r') as item_insert_file:
+    item_insert_query = item_insert_file.read()
 
 conn_string = "host='localhost' dbname='arbitrage' user='postgres'"
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 print("Connected to postgres!")
 h = httplib2.Http(".cache")
-'''
 persist regional data
 regions_response, content = h.request("https://esi.tech.ccp.is/latest/universe/regions/?datasource=tranquility", "GET")
 regions = literal_eval(content.decode('utf-8'))
@@ -34,8 +34,6 @@ for region in regions:
     cursor.execute(region_insert_query,(region_pretty['region_id'],region_pretty['description'],region_pretty['name']))
     conn.commit()
     print("Region:" + region_pretty['name'])
-'''
-'''
 #persist constellation data
 consellations_response, content = h.request("https://esi.tech.ccp.is/latest/universe/constellations/?datasource=tranquility", "GET")
 constellations = literal_eval(content.decode('utf-8'))
@@ -47,32 +45,43 @@ for constellation in constellations:
     cursor.execute(constellation_insert_query,(constellation_pretty['constellation_id'], constellation_pretty['region_id'], constellation_pretty['name']))
     conn.commit()
     print("Constellation:" + constellation_pretty['name'])
-'''
 #persist system data
 systems_response, systems_content = h.request("https://esi.tech.ccp.is/latest/universe/systems/?datasource=tranquility", "GET")
 systems = literal_eval(systems_content.decode('utf-8'))
 print("Queried Systems list from CCP!")
 for system in systems:
-    system_response, system_content = h.request("https://esi.tech.ccp.is/dev/universe/systems/"+ str(system) +"/?datasource=tranquility&language=en-us", "GET")
-    s_pretty = literal_eval(system_content.decode('utf-8'))
-    cursor.execute(system_insert_query,(s_pretty['system_id'],s_pretty['constellation_id'], s_pretty['name'], s_pretty['security_status']))
-    conn.commit()
-    print("System:" + s_pretty['name'])
-    #persist station data
-    if 'stations' in s_pretty:
-        cursor.execute(station_query,([s_pretty['system_id']]))
-        if cursor.fetchone() is None:
-            stations_in_system = s_pretty['stations']
-            for station in stations_in_system:
-                station_reponse, station_content = h.request("https://esi.tech.ccp.is/dev/universe/stations/"+ str(station) + "/?datasource=tranquility","GET")
-                station_pretty = literal_eval(station_content.decode('utf-8'))
-                if 'name' in station_pretty:
-                    cursor.execute(station_insert_query,(station_pretty['name'], station_pretty['station_id'],station_pretty['system_id']))
-                    conn.commit()
-                    print("Station:" + station_pretty['name'])
+    if system > 31001032:
+        system_response, system_content = h.request("https://esi.tech.ccp.is/dev/universe/systems/"+ str(system) +"/?datasource=tranquility&language=en-us", "GET")
+        s_pretty = literal_eval(system_content.decode('utf-8'))
+        cursor.execute(system_insert_query,(s_pretty['system_id'],s_pretty['constellation_id'], s_pretty['name'], s_pretty['security_status']))
+        conn.commit()
+        print("System:" + s_pretty['name'])
+        #persist station data
+        if 'stations' in s_pretty:
+            cursor.execute(station_query,([s_pretty['system_id']]))
+            if cursor.fetchone() is None:
+                stations_in_system = s_pretty['stations']
+                for station in stations_in_system:
+                    station_reponse, station_content = h.request("https://esi.tech.ccp.is/dev/universe/stations/"+ str(station) + "/?datasource=tranquility","GET")
+                    station_pretty = literal_eval(station_content.decode('utf-8'))
+                    if 'name' in station_pretty:
+                        cursor.execute(station_insert_query,(station_pretty['name'], station_pretty['station_id'],station_pretty['system_id']))
+                        conn.commit()
+                        print("Station:" + station_pretty['name'])
 
-#persist market group data
 
-
-#persist item data
-
+market_response, market_content = h.request("https://esi.tech.ccp.is/dev/markets/groups/?datasource=tranquility","GET")
+m_pretty = literal_eval(market_content.decode('utf-8'))
+for group in m_pretty:
+    group_response, group_content = h.request("https://esi.tech.ccp.is/dev/markets/groups/"+str(group) +"/?datasource=tranquility&language=en-us", "GET")
+    group_pretty = literal_eval(group_content.decode('utf-8'))
+    print(group_pretty)
+    if group_pretty['types']:
+        #persist item data
+        for item in group_pretty['types']:
+            print(item)
+            item_response, item_content = h.request("https://esi.tech.ccp.is/dev/universe/types/" + str(item)+"/?datasource=tranquility&language=en-us","GET")
+            item_pretty = json.loads(item_content.decode('utf-8'))
+            if item_pretty['published']:
+                cursor.execute(item_insert_query, (item_pretty['name'], item_pretty['type_id'], item_pretty['description']))
+                conn.commit()
