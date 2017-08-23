d69de9cdf60216c65769d51e6b464422a7d0bd56
vanrysss <samvanryssegem@gmail.com>
2017-08-22 20:24:51 -0700

    refactor to use requests and more sql shenanigans

--- arbitrage.py
+++ arbitrage.py
@@ -1,5 +1,5 @@
 import psycopg2
-import httplib2
+import requests
 import json
 from ast import literal_eval
 
@@ -16,22 +16,18 @@ conn_string = "host='localhost' dbname='arbitrage' user='postgres'"
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 print("Connected to postgres!")
-h = httplib2.Http(".cache")
 
 cursor.execute(all_regions_query)
 regions = cursor.fetchall()
 for region in regions:
-    item_id = 2445
-    while item_id <= 2625:
-        cursor.execute(select_item_by_id,([item_id]))
-        item_type_id = cursor.fetchone()
-        region_response, region_content = h.request("https://esi.tech.ccp.is/dev/markets/" + str(region[0]) +"/orders/?datasource=tranquility&order_type=all&page=1&type_id=" + str(item_type_id[0]),"GET")
-        items = json.loads(region_content.decode('utf-8'))
-        print(region_response.status)
-        print(region_content)
-        if region_response.status == 200:
-            for item in items:
-                if item['location_id'] < 61001146:
-                    cursor.execute(order_insert_query,(item['duration'], item['is_buy_order'], item['issued'], item['location_id'], item['min_volume'], item['order_id'], item['price'],item['range'], item['type_id'],item['volume_remain'],item['volume_total']))
-                    conn.commit()
-        item_id +=1
+    item_id = 44992
+    cursor.execute(select_item_by_id,([item_id]))
+    item_type_id = cursor.fetchone()
+    r = requests.get("https://esi.tech.ccp.is/dev/markets/" + str(region[0]) +"/orders/?datasource=tranquility&order_type=all&page=1&type_id=34828")
+    if r.status_code == 200:
+        for item in r.json():
+            if item['location_id'] < 61001146:
+                cursor.execute(order_insert_query,(item['duration'], item['is_buy_order'], item['issued'], item['location_id'], item['min_volume'], item['order_id'], item['price'],item['range'], item['type_id'],item['volume_remain'],item['volume_total']))
+                conn.commit()
+    else:
+        print(r.status_code)
--- sql/scratch.sql
+++ sql/scratch.sql
@@ -31,3 +31,31 @@ SELECT r.name,stat.name FROM station stat
     JOIN constellation c ON s.eve_constellation_id = c.eve_constellation_id
     JOIN region r ON c.eve_region_id = r.eve_region_id
     WHERE stat.eve_station_id = 60014945;
+
+#filter out non sec-space stations
+SELECT count(1), r.name FROM star_system sys
+    JOIN constellation c ON sys.eve_constellation_id = c.eve_constellation_id
+    JOIN region r ON c.eve_region_id = r.eve_region_id
+    WHERE sys.security_status > 0.0
+    GROUP BY r.name;    
+
+#core functionality query
+SELECT i.name, 
+        sale.volume_remain, 
+        buy.volume_remain, 
+        sale.price AS sale_price, 
+        buy.price AS buy_price, 
+        sale_stat.name AS sale_station, 
+        buy_stat.name AS buy_station, 
+        buy.eve_order_id, 
+        sale.eve_order_id,
+        (SELECT LEAST(buy.volume_remain, sale.volume_remain) * (buy.price - sale.price)) AS profit
+    FROM market_order sale
+    JOIN item i ON sale.item_type_id = i.type_id
+    JOIN market_order buy ON sale.item_type_id = buy.item_type_id
+    JOIN station buy_stat ON buy.location_id = buy_stat.eve_station_id
+    JOIN station sale_stat ON sale.location_id = sale_stat.eve_station_id
+    WHERE sale.is_buy_order = FALSE
+    AND buy.is_buy_order = TRUE
+    AND buy.price > sale.price
+    ORDER BY profit DESC;
